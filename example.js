f603089fc8fb9a12cfb73059d265932a25c23051
rocky_manobi <tak.koroki@gmail.com>
2017-09-02 02:10:20 +0900

    add express option

--- example.js
+++ example.js
@@ -1,13 +1,37 @@
 
+const express = require("express");
+const http = require("http");
+const app = express();
 
 const moment = require("moment");
 const mongoLog = require("./mongoLog");
 const url = process.env.MONGO_URL;
 
-mongoLog.putLog( { id: "test1", hoge: "example1" } , url );
+function extraQueryBuilder( req ){
+  return {};//{  segment: req.query.segment || "DA" };
+}
+function makeLine( i ){
+  return [ moment( i.timestamp ).format("YYYY/MM/DD HH:mm:ss"),
+    i.segment,
+    i.clientId,
+    i.requestId,
+    i.characterCalled,
+    i.userUtterance,
+    i.characterRespond,
+    i.botUtterance,
+    i.category ].join(",");
+}
+
+
+app.set( 'port', process.env.PORT || 3000 );
+app.get( '/', mongoLog.expressCsvLogRoute( extraQueryBuilder, makeLine, url ) );
+const server = http.createServer( app ).listen( app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
 
 mongoLog.getLog( {
-  fromDate: moment().set("hour", 0).toDate(),
+  fromDate: moment("20170901").set("hour", 0).toDate(),
   toDate: moment().toDate(),
   queryOptions: {}
 }, url )
--- mongoLog.js
+++ mongoLog.js
@@ -1,4 +1,5 @@
 
+const moment = require("moment");
 const MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
@@ -25,11 +26,13 @@ function getLog( options, url ){
 
   return new Promise((resolve, reject)=>{
 
+
     MongoClient.connect( url , function(err, db) {
       assert.equal(null, err);
       console.log("Connected correctly to server");
       queryOptions.timestamp = { $gt : fromDate.getTime(), $lt: toDate.getTime() };
 
+      console.log( queryOptions );
       const collection = db.collection('botlog');
       collection.find( queryOptions ).toArray(function(err, docs) {
         if( err ){ return reject(err); }
@@ -54,8 +57,50 @@ var insertDocuments = function(collectionName, value ,db, callback) {
   });
 }
 
+
+function expressCsvLogRoute( extraQueryBuilder, formatter, mongoUrl ){
+
+  return function(req,res){
+    console.log( "hello" );
+
+    const fromDate = moment( req.query.date ).set({ hour: 0, minute: 0, seconds: 0}).toDate();
+    const toDate = moment( req.query.date ).set({ hour: 23, minute: 59, seconds: 59}).toDate();
+    console.log( fromDate );
+    console.log( toDate );
+    console.log( extraQueryBuilder( req ) );
+  
+    getLog({
+      queryOptions : extraQueryBuilder( req ),
+      fromDate,
+      toDate,
+    }, mongoUrl).then((docs)=>{
+      //console.log(docs);
+      const lines = docs.map( doc => formatter( doc )).join("\r\n");
+      var jconv = require( 'jconv' );
+      res.attachment('filename.csv');
+  
+      if( req.query.code === "sjis" ){
+        res.setHeader( 'Content-Type', 'text/csv; charset=Shift_JIS' );
+        res.write( jconv.convert( lines , 'UTF8', 'SJIS' ) ) ;
+      }else{
+        res.setHeader( 'Content-Type', 'text/csv; charset=UTF-8' );
+        res.write( lines  );
+      }
+      res.end();
+    })
+    .catch((e)=>{
+      res.status(500);
+      res.send( JSON.stringify(e) );
+    });
+  };
+}
+
+
+
+
 module.exports = {
   putLog : putLog,
   getLog : getLog,
+  expressCsvLogRoute: expressCsvLogRoute,
 };
 
